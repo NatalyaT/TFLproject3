99304626c443e6de41384c2e32cd42b3e1639623
Natalya <tullochnatalya@gmail.com>
2019-02-23 20:37:14 +0000

    Second Commit

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+let imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray.src = 'assets/rock.png';
+
+imgArray[1] = new Image();
+imgArray.src = 'assets/paper.png';
+
+imgArray[2] = new Image();
+imgArray.src = 'assets/scissors.png';
+
+function display(userChoice, compChoice){
+    let userPlace = document.getElementById('userPlace')
+    let compPlace = document.getElementById('compPlace')
+    
+    userPlace.setAttribute('src', imgArray[userChoice].src);
+    compPlace.setAttribute('src', imgArray[compChoice].src);
+}
+
 function randNum(){
    return Math.floor(Math.random() * 3);
 }
 
 function checkResult(userChoice){
-    let document.getElementById("result")
+    let result = document.getElementById("result")
+    console.log(result)
     let compChoice = randNum()
+    
+    display(userChoice, compChoice)
     
     if (userChoice == compChoice){
         result.innerHTML = "You tied"
